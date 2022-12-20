e670dbeab4be66f51758a5449a8767c174755b0a
ShihTingJustin <justinhuang777@gmail.com>
2022-12-20 15:36:54 +0800

    chore: update absolute path

--- src/routes/common.tsx
+++ src/routes/common.tsx
@@ -1,10 +1,10 @@
 import React, { Suspense } from 'react';
 import { RouteObject } from 'react-router-dom';
 
-import ErrorBoundary from '@/Pages/error';
-import Spinner from '@/Components/spinner';
+import ErrorBoundary from '@Pages/error';
+import Spinner from '@Components/spinner';
 
-const Home = React.lazy(() => import('@/Pages/home'));
+const Home = React.lazy(() => import('@Pages/home'));
 
 export const common: RouteObject[] = [
   {
