5c963b703365e4d634f5de7d6e9a6d6f5f4e808e
ShihTingJustin <justinhuang777@gmail.com>
2023-04-11 22:20:14 +0800

    chore: replace firebase with github pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { common } from './common';
 
-export const allRouters = createBrowserRouter([...common]);
+export const allRouters = createBrowserRouter([...common], {
+  basename: process.env.REACT_APP_PUBLIC_URL,
+});
